7a81275171a11fe8a9184821851466b59459f6d8
Matthew Mulholland <mmulholland@ets.org>
2020-07-10 23:24:35 -0400

    Add front end

---
+++ build/precache-manifest.143c42c7d5d339f00ce6ad6454ea1686.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "921fafed2cdecfd70f4cd7ed8c440297",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a90a3a98a5f9b423717",
+    "url": "/static/css/main.adb0e0ae.chunk.css"
+  },
+  {
+    "revision": "261379cd12f7a257a30b",
+    "url": "/static/js/2.72903e02.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.72903e02.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a90a3a98a5f9b423717",
+    "url": "/static/js/main.1459175f.chunk.js"
+  },
+  {
+    "revision": "8dd7441d529e73d61d47",
+    "url": "/static/js/runtime-main.aaa24d6c.js"
+  }
+]);
